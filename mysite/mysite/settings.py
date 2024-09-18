2fa654c97947f2f23624f87a1287fb4e33040172
andredias77 <andre.terra@usp.br>
2024-09-18 14:13:09 -0300

    Deploy final??

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kpzh4ky_ew+-181hl0dltxi&p2(ggbz+*2x@hwa!3x#k#^*8vm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
